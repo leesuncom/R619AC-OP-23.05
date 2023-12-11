04dc6791fa23c38a817e06e97a2f1906c599a32e
leesuncom <70786826+leesuncom@users.noreply.github.com>
2023-12-11 16:47:29 +0800

    Add files via upload

---
+++ diy-part2.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+echo "uci -q batch << EOI
+set network.lan.ipaddr='192.168.10.1'
+commit network
+set wireless.radio0.htmode='HT20'
+set wireless.radio0.band='2g'
+set wireless.radio0.channel='auto'
+set wireless.@wifi-iface[0].ssid='OpenWrt2023'
+set wireless.@wifi-device[0].disabled='0'
+set wireless.radio1.htmode='VHT40'
+set wireless.radio1.band='5g'
+set wireless.radio1.channel='auto'
+set wireless.@wifi-iface[1].ssid='OpenWrt2023-5G'
+set wireless.@wifi-device[1].disabled='0'
+commit wireless
+set system.@system[-1].timezone='CST-8'
+set system.@system[-1].zonename='Asia/Shanghai'
+commit system
+del dhcp.lan.ra
+del dhcp.lan.ra_slaac
+del dhcp.lan.ra_flags
+del dhcp.lan.dhcpv6
+commit dhcp
+EOI" > package/base-files/files/etc/uci-defaults/99-custom
+#eof
